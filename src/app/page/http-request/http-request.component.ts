eabfd191ce4467bd90cf501c3eb94a48e87c6b7f
15akash <40172763+15akash@users.noreply.github.com>
2022-09-18 17:24:21 +0530

    Add files via upload

--- src/app/page/http-request/http-request.component.ts
+++ src/app/page/http-request/http-request.component.ts
@@ -34,7 +34,6 @@ export class HttpRequestComponent implements OnInit {
 
   isLoading: boolean = false;
 
-  charactersArr: string[] = [];
   charactersList: I_character[] = [];
   charactersId: string[] = [];
 
@@ -49,52 +48,63 @@ export class HttpRequestComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.getAllEpisodes();
   }
 
-  getAllEpisodes() {
+  async getAllEpisodes() {
     if (this.episodesPageCount > this.totalEpisodesPageCount) {
       this.charactersId = [...new Set(this.charactersId)];
-      return this.getAllCharacters();
+      return;
     } else {
       const episodesBaseUrl = `https://rickandmortyapi.com/api/episode/?page=${this.episodesPageCount}`;
-      fetch(episodesBaseUrl).then(response => response.json()).then((response) => {
+      const response: any = await fetch(episodesBaseUrl).then(response => response.json())
+      try {
         // pushing the data in episodes list.
         this.episodesList.push(...response.results);
         this.totalEpisodesPageCount = response.info.pages;
+        let charactersUrl: string[] = [];
         for (let i = 0; i < this.episodesList.length; i++) {
           let charactersInOneEpisode: string[] = this.episodesList[i].characters;
           // creating characters Url array received from this api
-          this.charactersArr.push(...charactersInOneEpisode);
+          charactersUrl.push(...charactersInOneEpisode);
         }
-        this.charactersArr.forEach(item => {
+
+        charactersUrl.forEach(item => {
           let id = item.lastIndexOf('/') + 1;
           let result = item.substring(id, item.length);
           // creating characters ID as array of string to get all the characters information in getAllCharacters function.
           this.charactersId.push(result);
         })
-        this.episodesPageCount += 1;
-        this.getAllEpisodes();
-      }).catch((error) => console.log(error));
+        this.episodesPageCount++;
+        await this.getAllEpisodes();
+      } catch (error) {
+        alert(error)
+        console.log(error)
+      }
 
     }
   }
 
-  getAllCharacters() {
+  async getAllCharacters() {
     const baseUrlForCharacters = `https://rickandmortyapi.com/api/character/${this.charactersId}`;
-    fetch(baseUrlForCharacters).then(response => response.json()).then((response) => {
+    const response: any = await fetch(baseUrlForCharacters).then(response => response.json())
+    try {
       for (let character of response) {
         // created character list and pushing all the characters received from this api.
         this.charactersList.push(character)
         this.charactersObj[character.id] = character;
       }
-    }).catch((error) => console.log(error))
+    } catch (error) {
+      alert(error)
+      console.log(error)
+    }
   }
 
 
-  modifyEpisodeArr() {
+  async modifyEpisodeArr() {
     if (this.modifiedEpisodesList.length) return;
     this.isLoading = true;
+    await this.getAllEpisodes();
+    await this.getAllCharacters();
     for (let i = 0; i < this.episodesList.length; i++) {
       this.episodesList[i].characters.forEach((item, index) => {
         let lastIndex = item.lastIndexOf('/') + 1;
